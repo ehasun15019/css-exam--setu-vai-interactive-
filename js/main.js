943d54f59f8e022849a6d7682d53c3b85253d059
Ehasun Ul Islam <ehasun0909@gamil.com>
2025-03-22 18:02:16 +0600

    22march-initial

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// cart.html img slider start
+function imgGallery() {
+    const mainImg = document.querySelector('.details_img');
+    const smallImg = document.querySelectorAll('.details_small-img');
+    
+    smallImg.forEach((img) => {
+        img.addEventListener('click', function(){
+        mainImg.src = this.src;
+        })
+    });
+ }
+    
+imgGallery();
+
+
+
+
+// increment-decrement-sec start
+let counter = 1;
+const counterElement = document.getElementById("counter");
+const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
+
+incrementButton.addEventListener("click", () => {
+    counter++;
+    counterElement.textContent = counter;
+    decrementButton.classList.remove("disabled"); // Enable decrement button
+});
+
+decrementButton.addEventListener("click", () => {
+    if (counter > 1) {
+        counter--;
+        counterElement.textContent = counter;
+        if (counter === 1) {
+            decrementButton.classList.add("disabled"); // Disable if 1
+        }
+    }
+});
+// increment-decrement-sec end
+// cart.html slider end
